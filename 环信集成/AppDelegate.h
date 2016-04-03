da2a057f30963f302cd3d17fbb3d4f24dd850a5b
wangtao <764765614@qq.com>
2016-04-03 20:10:37 +0800

    Initial Commit

---
+++ 环信集成/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  环信集成
+//
+//  Created by 王涛 on 16/4/3.
+//  Copyright © 2016年 304. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 环信集成/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  环信集成
+//
+//  Created by 王涛 on 16/4/3.
+//  Copyright © 2016年 304. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
